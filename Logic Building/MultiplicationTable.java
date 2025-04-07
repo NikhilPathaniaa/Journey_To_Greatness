4239c959557a1d336f9816a4d7d28086d02f5a7c
NikhilPathaniaa <developernikhil1@gmail.com>
2025-04-07 21:24:50 +0530

    MultiplicationTable

---
+++ Logic Building/MultiplicationTable.java
@@ -0,0 +1,10 @@
+class MultiplicationTable {
+    public static void main(String[] args){
+        int num  = 7;
+
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(num+" * "+i" = "+i*num);
+        }
+    }
+}
